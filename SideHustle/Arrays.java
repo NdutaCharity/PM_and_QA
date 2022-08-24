a0e7479b3a8e5488e385f34079c9d955c1e99338
NdutaCharity <58165250+NdutaCharity@users.noreply.github.com>
2022-08-24 13:07:06 +0300

    Add files via upload

---
+++ SideHustle/Arrays.java
@@ -0,0 +1,15 @@
+package arrays;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		
+		int numb = 10;
+		
+		int[] number = {10,15,20,25,30,35,40};
+		
+		System.out.println(number[2]);
+		System.out.println(number[5]);
+
+	}
+}
---
+++ SideHustle/Forloop.java
@@ -0,0 +1,15 @@
+package forloop;
+
+public class Forloop {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0; i <= 2; i++) {
+			System.out.println("SideHustle " + i);
+		}
+		
+		for(double i = 0.0; i <= 2.0; i++) {
+			System.out.println("SideHustle " + i);
+		}
+	}
+}
